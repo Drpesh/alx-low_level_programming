b7c235b4401ae13c125ea9126b8780bf13d303a8
okafor chiamaka <egeonuchiamaka11@gmail.com>
2022-09-09 14:36:06 -0700

    task 05

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 
+
+
 /**
-* main - main block
-* Return: 0
+*main - prints the phrase with proper grammar,
+*but the outcome is a piece of art,
+*Return: 0 if exited properly, non-zero otherwise
 */
+
 int main(void)
+
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
 	return (0);
+
 }
