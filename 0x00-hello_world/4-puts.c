27d9e0cdbde96b3b247d8eb6681ad7ec86d93cb0
okafor chiamaka <egeonuchiamaka11@gmail.com>
2022-09-08 19:36:55 -0700

    task 04

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,27 +4,25 @@
 
 /**
 
- * main - prints the phrase "Programming is like building a multilingual puzzle
+* main - print the string in the put function
 
- * Return: 0 if exited properly, non-zero otherwise
+*
 
- */
+* Description: using the main function
 
+* this program prints "Programming is like building a multilingual puzzle
 
+* Return: 0
+
+*/
 
 int main(void)
 
-
-
 {
 
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
+	return (0);
 
-		puts("\"Programming is like building a multilingual puzzle");
-
-
-
-			return (0);
-
-
+}
 
