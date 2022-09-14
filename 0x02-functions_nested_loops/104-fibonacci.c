60a015d5b4d65e408013f40492eae834c083ce28
okafor chiamaka <egeonuchiamaka11@gmail.com>
2022-09-14 03:06:31 -0700

    task 104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,48 +1,45 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * main - Entry point that prints the first 98 Fibonacci numbers
- *
- *Return: Always 0 (Success)
+ * main - finds and prints the first 98 Fibonacci numbers,
+ * starting with 1 and 2
+ * followed by a new line
+ * Return: ALways 0 (Success)
  */
 
 int main(void)
-{
-	int i, res;
-	unsigned long int ant = 0, antt = 0;
-	unsigned long int new = 0, newt = 0;
-	unsigned long int sum = 1, sumt = 0;
 
-	for (i = 0; i < 92; i++)
+{
+	unsigned long int i, j, k, j1, j2, k1, k2;
+
+	j = 1;
+	k = 2;
+
+	printf("%lu", j);
+
+	for (i = 1; i < 91; i++)
 	{
-		new = sum + ant;
-		printf("%lu", new);
-		ant = sum;
-		sum = new;
-		printf(", ");
+		printf(", %lu", k);
+		k = k + j;
+		j = k - j;
 	}
-	antt = ant % 1000000000;
-	ant = ant / 1000000000;
-	sumt = sum % 1000000000;
-	sum = sum / 1000000000;
-	for (i = 92; i < 98; i++)
+
+	j1 = j / 1000000000;
+	j2 = j % 1000000000;
+	k1 = k / 1000000000;
+	k2 = k % 1000000000;
+
+	for (i = 92; i < 99; ++i)
 	{
-		new = sum + ant;
-		newt = sumt + antt;
-		res = newt / 1000000000;
-		if (res != 0)
-		{
-			new = new + res;
-			newt = newt % 1000000000;
-		}
-		printf("%lu%lu", new, newt);
-		ant = sum;
-		antt = sumt;
-		sum = new;
-		sumt = newt;
-		if (i < 97)
-			printf(", ");
+		printf(", %lu", k1 + (k2 / 1000000000));
+		printf("%lu", k2 % 1000000000);
+		k1 = k1 + j1;
+		j1 = k1 - j1;
+		k2 = k2 + j2;
+		j2 = k2 - j2;
 	}
-	putchar('\n');
+
+	printf("\n");
+
 	return (0);
 }
